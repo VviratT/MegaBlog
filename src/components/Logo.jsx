4eb552e58738d3f7f0cc5a10e14ddc2af748aa29
VviratT <borbanayush09@gmail.com>
2025-05-21 00:16:24 +0530

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Logo({ width = "100px" }) {
   return (
     <img
-      src="../../logo2.png"
+      src="/logo2.png"
       alt="Logo"
       style={{ width }}
       className="rounded-full shadow-2xl transform h-auto bg-oragne-400 w-auto transition duration-200 hover:scale-105 hover:bg-orange-600"
